59d79a87aa8a946c5e961e35c6e381ab55f684fa
hung20gg <hung20gg@gmail.com>
2024-04-12 02:10:10 +0700

    model trainable

--- training/architecture/tf/model.py
+++ training/architecture/tf/model.py
@@ -1,9 +1,13 @@
-from .efficientnet import MBBlock
-from .transformer import TransformerBlock
+from efficientnet import MBBlock
+from transformer import TransformerBlock
+import tensorflow as tf
 
 CHANNELS = 66
+PAD = -100
+NUM_CLASSES = 250
 
-def get_model(max_len=64, dropout_step=0, dim=192, ):
+
+def get_model(max_len=64, dim=192 ):
     inp = tf.keras.Input((max_len,CHANNELS))
     x = tf.keras.layers.Masking(mask_value=PAD,input_shape=(max_len,CHANNELS))(inp)
     ksize = 17
@@ -20,19 +24,13 @@ def get_model(max_len=64, dropout_step=0, dim=192, ):
     x = MBBlock(dim,ksize,drop_rate=0.2)(x)
     x = TransformerBlock(dim,expand=2)(x)
 
-    if dim == 384: #for the 4x sized model
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = TransformerBlock(dim,expand=2)(x)
-
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = MBBlock(dim,ksize,drop_rate=0.2)(x)
-        x = TransformerBlock(dim,expand=2)(x)
-
     x = tf.keras.layers.Dense(dim*2,activation=None,name='top_conv')(x)
     x = tf.keras.layers.GlobalAveragePooling1D()(x)
-    x = LateDropout(0.8, start_step=dropout_step)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
     x = tf.keras.layers.Dense(NUM_CLASSES,name='classifier')(x)
     return tf.keras.Model(inp, x)
+
+if __name__ == "__main__":
+    x = tf.random.normal((32, 100, 66))
+    model = get_model()
+    model.summary()
